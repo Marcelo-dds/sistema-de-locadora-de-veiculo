ed991ea41f5a963ecc82ab124889f3afbb683305
marcelo-dds <marcelodds_web@hotmail.com>
2024-10-29 13:00:10 -0300

    parte de continuar e encerrar o sistema funcionando

--- locadoradecarro/locadora.py
+++ locadoradecarro/locadora.py
@@ -12,6 +12,32 @@ carros = [("Chevrolet tracker", 120),
 alugados = []
 
 
-print('==========')
-print('Bem vindo à locadora de carros!.')
-print('==========')
+
+
+def mostrar_lista_carros(lista_de_carros):
+    for i,car in enumerate(lista_de_carros):
+        print("[{}] {} - R$ {} / dia".format(i,car[0], car[1]))
+
+mostrar_lista_carros(carros)
+
+while True:
+    os.system('cls')
+    print('==========')
+    print('Bem vindo à locadora de carros!.')
+    print('==========')
+    print('O que deseja fazer? ')
+    print('0 - Mostrar portifólio | 1 - Alugar um carro | 2 - Devolver um carro ')
+    op = int(input())
+    os.system('cls')
+    if op == 0:
+        pass
+    elif op == 1:
+        pass
+    elif op == 2:
+        pass
+
+    print("")
+    print("==========")
+    print('Digite 0 para CONTINUAR || Digite 1 para SAIR')
+    if float(input()) == 1:
+        break
