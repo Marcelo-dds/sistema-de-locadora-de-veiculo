3b303eeb6067a841f2106d5955edc4c894336f82
marcelo-dds <marcelodds_web@hotmail.com>
2024-10-29 12:31:38 -0300

    criado a listas de carros uma lista d tuplas

--- locadoradecarro/locadora.py
+++ locadoradecarro/locadora.py
@@ -1,1 +1,17 @@
 import os
+
+carros = [("Chevrolet tracker", 120),
+          ("Chevrolet Onix" , 90),
+          ("Chrevolet spin" , 180),
+          ("Hyundai HB20" , 85),
+          ("Hyundai Tucson" , 120),
+          ("Fiat Uno" , 60),
+          ("Fiat Mobi" , 70),
+          ("Fiat Pulse" , 130)
+          ]
+alugados = []
+
+
+print('==========')
+print('Bem vindo à locadora de carros!.')
+print('==========')
